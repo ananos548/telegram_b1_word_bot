1483dc1e40c4d44c1e97dc665e3be23eddd03bb0
ananos548 <inabsm@mail.ru>
2023-09-30 17:49:59 +0300

    Word restart system

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+all_pairs = []
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    global all_pairs
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn = types.KeyboardButton("Send random b1 english word")
     markup.row(btn)
@@ -22,12 +25,17 @@ def start(message):
 
 @bot.message_handler(func=lambda message: message.text == "Send random b1 english word")
 def send_random_pair(message):
-    session = session_maker()
-    query = select(Word.word, TranslatedWord.translated_word).select_from(join(Word, TranslatedWord)).where(
-        TranslatedWord.word_id == Word.id)
-    pairs = session.execute(query).fetchall()
-    if pairs:
-        pair = pairs[random.randint(0, len(pairs) - 1)]
+    global all_pairs
+
+    # Если список всех пар пуст или все пары были отправлены, перезаполняем его
+    if not all_pairs:
+        session = session_maker()
+        query = select(Word.word, TranslatedWord.translated_word).select_from(join(Word, TranslatedWord)).where(
+            TranslatedWord.word_id == Word.id)
+        all_pairs = session.execute(query).fetchall()
+        random.shuffle(all_pairs)  # Перемешиваем пары для случайного порядка
+    if all_pairs:
+        pair = all_pairs.pop()  # Извлекаем и удаляем пару из списка
         bot.send_message(message.chat.id, f"{pair[0]}, {pair[1]}")
 
 
